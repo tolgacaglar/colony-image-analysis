3d2474e2057c4ee9686b9542abbc272b53398b1f
Tolga Caglar <tolcaglar@gmail.com>
2020-12-03 22:05:38 -0800

    as of thursday night

--- colony.py
+++ colony.py
@@ -1,3 +1,9 @@
 """
-
+The module that keeps the Colony and XML classes
 """
+
+class Colony:   # Class that keeps a single colony
+    def __init__(self, img_path_ar, metadata_path_ar):    # Initialize colony object
+        self.img_path_ar = img_path_ar.copy()
+        self.metadata_path_ar = metadata_path_ar.copy()
+    def combine(self):  # combines the images
