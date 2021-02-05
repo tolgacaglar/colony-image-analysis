d4e99fd915b82b90ba9c777ce48ad7eda2cdc9f3
Tolga Caglar <tolcaglar@gmail.com>
2021-02-05 12:45:44 -0800

    timepoint planning is finalized

--- 01032021/colony.py
+++
@@ -1,8 +0,0 @@
-"""
-The module that keeps the Colony and XML classes
-"""
-
-class Colony:   # Class that keeps a single colony
-    def __init__(self, img_path_ar):    # Initialize colony object
-        self.img_path_ar = img_path_ar
-    def combine(self):  # combines the images
